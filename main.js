9952235745ea388e8220c347ed13e9f2b2c7ee4a
sapce-microwave <bashir_panjshiri@hotmail.com>
2023-01-12 16:53:02 +0100

    the object.entries() methode

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const object = {
-    name: "michel",
-    lastName: "jackson",
-    country: "the world"
-}
+  name: "michel",
+  lastName: "jackson",
+  country: "the world",
+};
 
-console.log(object)
+console.log(object);
+
+// this methode will return array of arrays 
+// where each sub array contains the key-value pairs of the object
+console.log(Object.entries(object));
