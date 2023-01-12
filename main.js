bc09ab8f36cfd68af2264194b93295bbf2c14b27
sapce-microwave <bashir_panjshiri@hotmail.com>
2023-01-12 17:24:16 +0100

    dot notation adds new value to object

--- main.js
+++ main.js
@@ -4,12 +4,22 @@ const object = {
   country: "the world",
 };
 
-console.log(object);
+// using the dot notation will add new value to th object
+console.log(object.dateOfBirth = "1992");
+console.log(object)
 
 // this methode will return array of arrays 
 // where each sub array contains the key-value pairs of the object
-console.log(Object.entries(object));
+// console.log(Object.entries(object));
 
 // the keys methode will turn the object in to a array
-console.log(Object.keys(object));
+// console.log(Object.keys(object));
 
+
+
+// let array = []
+
+// for (let i = 0; i < object.length; i++) {
+//     console.log(object[i])
+    
+// }
